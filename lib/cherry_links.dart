6b053a2e0adba4d03156fe0748765a706c89faa1
Erick Zanardo <erickzanardoo@gmail.com>
2025-11-05 16:53:26 -0300

    adding g4me.info profile link

--- lib/cherry_links.dart
+++ lib/cherry_links.dart
@@ -2,4 +2,5 @@ abstract class CherryLinks {
   static const website = 'https://cherrybit.studio/';
   static const supportUs = 'https://cherrybit.studio/about-supporting/';
   static const discordInvite = 'https://discord.gg/Sb7wmcfJH2';
+  static const g4meProfileLink = 'https://g4me.info/@cherrybit';
 }
